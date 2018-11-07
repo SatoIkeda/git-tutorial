72c4b14ab4c222524cd3875d01063c9cdf76ed5a
Ikeda <j1503@matsue-ct.jp>
2018-11-07 11:30:29 +0900

    ADD master

---
+++ fizzbuzzgit.rb
@@ -0,0 +1,14 @@
+
+for i in 1..100 do
+	if i%3==0 && i%5==0 then
+		print("FizzBuzz\n")
+	elsif i%3==0 then
+		print("Fizz\n")
+	elsif i%5==0 then
+		print("Buzz\n")
+	elsif i%7==0 then 
+		print("git\n")	
+	else
+		print(i,"\n")
+	end
+end
